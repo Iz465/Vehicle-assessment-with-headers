7fcdb177f52cf6575e1097be14b29bc5f181de5d
Isak <126728670+Iz465@users.noreply.github.com>
2023-11-09 13:18:24 +1300

    Removing globals not needed

--- Vehicle assessment with headers.cpp
+++ Vehicle assessment with headers.cpp
@@ -17,38 +17,6 @@ using namespace std;
 
 
 
-/*string first;
-string last;
-string birth;
-string phone;
-string email;
-string address;
-string vehicle;
-string username;
-string password;
-string password1;
-char administrator;
-string admin_code;
-string authorised;
-string line;
-string gender;
-int policy_number;
-string vehicleNumber;
-string vehicleType;
-int chosenPolicy;
-string policyType;
-int motorbike = 70;
-int car = 80;
-int truck = 90;
-int boat = 100;
-int chosenVehicle;
-int fire = 50;
-int theft = 50;
-int carCrash = 60;
-int disastor = 50;
-int price;
-int answer;
-*/
 
 
 
@@ -62,6 +30,6 @@ int main()
 	start();
 
 
-
+	
 	system("pause>0");
 }
